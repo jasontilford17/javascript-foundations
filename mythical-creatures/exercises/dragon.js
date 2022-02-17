f1a46105cc7c4c998e49e6b8076d35dde2331648
jasontilford <97364044+jasontilford17@users.noreply.github.com>
2022-02-17 11:42:34 -0500

    completed dragon files
    has a dragon, rider, and can be hungry and eat

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name
+        this.rider = rider
+        this.hungry = true
+        this.timesEaten = 0   
+    }
 
+    greet(rider) {
+        return `Hi, ${this.rider}!`
+    }
+    
+    eat() {
+        this.timesEaten++
+        if(this.timesEaten > 2) {
+            this.hungry = false
+        }
+    }
+}
+
+module.exports = Dragon
